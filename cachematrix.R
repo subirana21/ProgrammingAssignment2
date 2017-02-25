428b0b76cd4676dbf60085704ee34be151012c6f
subirana21 <subirana21@gmail.com>
2017-02-25 15:54:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+#TEST
+######################################################
+#> source('C:/Users/PcCom/Desktop/Exercicis R/funcions.R')
+#> matriu<- matrix(1:4,2,2)
+#> my_matrix<-makeCacheMatrix(matriu)
+#> my_matrix$get()
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> my_matrix$getInverse()
+#NULL
+#> cacheSolve(my_matrix)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(my_matrix)
+#Getting data from cache...
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#
+##################################################
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  setInverse <- function(invers) inv <<- solve(invers)
+  
+  getInverse <- function() inv
+  
+  list(set=set,get=get,setInverse=setInverse,
+       getInverse=getInverse )
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("Getting data from cache...")
+    return(inv)
+  }
+  
+  matr<- x$get()
+  inv <- x$setInverse(matr)
+  inv
 }
